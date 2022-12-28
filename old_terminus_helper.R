892dfe2db15eeb2d7944a5630c84c5307c991610
NPSDC <noorpratap@gmail.com>
2022-12-28 11:41:01 -0500

    length support for terminus

--- old_terminus_helper.R
+++ old_terminus_helper.R
@@ -2,13 +2,34 @@ library(fishpond)
 library(fishpond)
 library(SummarizedExperiment)
 
-parseClustFile <- function(file, y)
+replaceMissingLength <- function(lengthMat, aveLengthSampGroup) {
+    nanRows <- which(apply(lengthMat, 1, function(row) any(is.nan(row))))
+    if (length(nanRows) > 0) {
+        for (i in nanRows) {
+            if (all(is.nan(lengthMat[i, ]))) {
+                # if all samples have 0 abundances for all tx, use the simple average
+                lengthMat[i, ] <- aveLengthSampGroup[i]
+            } else {
+                # otherwise use the geometric mean of the lengths from the other samples
+                idx <- is.nan(lengthMat[i, ])
+                lengthMat[i, idx] <- exp(mean(log(lengthMat[i, !idx]), na.rm = TRUE))
+            }
+        }
+    }
+    lengthMat
+}
+
+
+parseClustFile <- function(file, y, gsub=F)
 {
     if(!file.exists(file))
         stop("Invalid file")
     df <- read.delim(file, header=F)
     groups <- lapply(strsplit(df$V1, split = ",", fixed = T), function(x) {
-            inds <- match(unlist(x[2:length(x)]), rownames(y))
+            if(gsub)
+                inds <- match(unlist(gsub("\\.[0-9]+", "", x[2:length(x)])), rownames(y))
+            else
+                inds <- match(unlist(x[2:length(x)]), rownames(y))
             #print(inds)
             if(sum(is.na(inds) > 0))
                 stop("error ")
@@ -16,7 +37,7 @@ parseClustFile <- function(file, y)
         })
     names(groups) <- as.character(nrow(y) + 1:length(groups))
     return(groups)
-    
+
 }
 
 computeOAggNodesU <- function(groups, nodeID, se_counts, group_inds = NULL) {
@@ -25,13 +46,13 @@ computeOAggNodesU <- function(groups, nodeID, se_counts, group_inds = NULL) {
             stop("counts has to be matrix/dataframe")
         if(is.null(rownames(counts)))
             stop("rows must be named")
-        
+
         if(is.null(col_inds))
             return(counts)
-        
+
         else
         {
-            
+
             df <- matrix(0, nrow = nrow(counts), ncol = length(col_inds), dimnames = list(rownames(counts)))
             for(i in seq_along(col_inds))
                 df[,i] = rowMeans(counts[,col_inds[[i]]])
@@ -52,14 +73,14 @@ computeOAggNodesU <- function(groups, nodeID, se_counts, group_inds = NULL) {
         rownames(df) <- names(row_inds)
         return(df)
     }
-    
+
     if(!is.numeric(nodeID))
     {
         nodeID <- as.numeric(nodeID)
         if(sum(is.na(nodeID)) > 0)
             stop("Node ids contain a non numeric")
     }
-    
+
     mat <- matrix(0, nrow=0, ncol=ncol(se_counts))
 
     leaves <- which(nodeID <= nrow(se_counts))
@@ -71,21 +92,34 @@ computeOAggNodesU <- function(groups, nodeID, se_counts, group_inds = NULL) {
         mat <- performOColAgg(se_counts[nodeID[leaves],])
     if(length(innNodes) > 0)
         mat <- rbind(mat, performOColAgg(se_counts, lInds))
-    
-    mat <- performORowAgg(mat, group_inds)       
-    
+
+    mat <- performORowAgg(mat, group_inds)
+
     return(mat)
 }
 
-prepOSwish <- function(y, inds, groups) 
+prepOSwish <- function(y, inds, groups)
 {
     asList <- vector(mode = "list", length(assays(y)))
     #asList <- vector(mode = "list", 5)
     names(asList) <- assayNames(y)
-    
-    for(n in names(asList))
-        asList[[n]] <- computeOAggNodesU(groups, inds, assays(y)[[n]], NULL)
-    
+
+    for(n in names(asList)) {
+        if(n!="length")
+            asList[[n]] <- computeOAggNodesU(groups, inds, assays(y)[[n]], NULL)
+        else {
+            abundLength <- assays(y)[["abundance"]] * assays(y)[["length"]]
+            weightedLength <- computeOAggNodesU(groups, inds, abundLength, NULL)
+            lengthMat <- weightedLength / asList[["abundance"]]
+            aveLengthSamp <- rowMeans(assays(y)[["length"]])
+            gs <- c(lapply(seq(nrow(y)), function(i) i), groups)
+            aveLengthSampGroup <- sapply(gs, function(ind) mean(aveLengthSamp[ind]))
+            lengthMat <- replaceMissingLength(lengthMat, aveLengthSampGroup)
+            asList[[n]] <- lengthMat
+        }
+
+    }
+
     y <- SummarizedExperiment(assays = asList, colData = colData(y), metadata = metadata(y))
     metadata(y)$infRepsScaled=F
     y
@@ -99,4 +133,3 @@ extractTxpGroup <- function(yAll, y, groups, diffTxpInds, mInds, txpSpace = c("y
     dtAll <- c(match(rownames(yAll)[dtes], rownames(y)), unlist(groups[dgroups]))
     return(dtAll)
 }
-    
